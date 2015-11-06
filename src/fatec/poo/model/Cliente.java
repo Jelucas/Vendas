9e01d1287968ec7166922c97d7e292bcadc41400
Jefferson Lucas <jeff.aplucas@gmail.com>
2015-11-06 14:06:33 -0200

    Associação entre Pedido, Cliente e Vendedor

--- src/fatec/poo/model/Cliente.java
+++ src/fatec/poo/model/Cliente.java
@@ -1,5 +1,28 @@
 package fatec.poo.model;
 
-public class Cliente {
+import java.util.ArrayList;
+
+public class Cliente extends Pessoa{
+    private ArrayList<Pedido> pedidos;
+    private double limiteCred;
+    private double limiteDisp;
     
+    public Cliente(String cpf, String nome,double limiteCred, double limiteDisp){
+        super(cpf,nome);
+        this.limiteCred = limiteCred;
+        this.limiteDisp = limiteDisp;
+        pedidos = new ArrayList<Pedido>();
+    }
+    
+    public void addCompra(double compra){
+        limiteDisp-= compra;
+    }
+    
+    public ArrayList<Pedido> getPedidos(){
+        return pedidos;
+    }
+    
+    public void addPedidos(Pedido p){
+        pedidos.add(p);
+    }
 }
--- src/fatec/poo/model/Vendedor.java
+++ src/fatec/poo/model/Vendedor.java
@@ -1,5 +1,19 @@
 package fatec.poo.model;
 
+import java.util.ArrayList;
+
 public class Vendedor {
+    private ArrayList<Pedido> pedidos;
     
+   public Vendedor(){
+       pedidos = new ArrayList<Pedido>();
+    }
+    
+    public ArrayList<Pedido> getPedidos(){
+        return pedidos;
+    }
+    
+    public void addPedidos(Pedido p){
+        pedidos.add(p);
+    }
 }
